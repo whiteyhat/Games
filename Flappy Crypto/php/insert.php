3ead13a25ec7d397f8705cc5982a22a7467c5618
WhiteyHat <carlosroldan26396@gmail.com>
2018-08-04 16:01:48 +0200

    Node modules have been removed

--- Flappy Crypto/php/insert.php
+++
@@ -1,9 +0,0 @@
-<?php
-$id = $_POST["id"]
-$score = $_POST["score"]
-require("Connection.php");
-$sql = "INSERT INTO satodb (id, age) VALUES ('$id', '$score')";
-
-mysqli_set_charset($connection, $sql);
-echo 1;
-?>
