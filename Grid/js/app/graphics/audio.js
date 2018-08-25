ea94575206f351831ff8001ff35c46256199b81e
WhiteyHat <carlosroldan26396@gmail.com>
2018-08-26 01:00:05 +0200

    Code reformatted correctly

--- Grid/js/app/graphics/audio.js
+++ Grid/js/app/graphics/audio.js
@@ -1,14 +1,14 @@
-define(['app/eventmanager', 'app/gamestate'], function(E, State) {
-	
+define(['app/eventmanager', 'app/gamestate'], function (E, State) {
+
 	var xPos3d = /matrix3d\(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, ([0-9\.]+)/;
 	var xPos = /matrix\(1, 0, 0, 1, ([0-9\.]+)/;
 	var G = null;
-	
+
 	var _el = null;
 	var musicHandle = null;
 	var effectsHandle = null;
 	function el() {
-		if(_el == null) {
+		if (_el == null) {
 			_el = G.make('volumeControls');
 			var musicSlider = makeSlider('music');
 			var effectsSlider = makeSlider('effects');
@@ -19,72 +19,72 @@ define(['app/eventmanager', 'app/gamestate'], function(E, State) {
 		}
 		return _el;
 	}
-	
+
 	function makeSlider(type) {
 		var slider = G.make(type + 'Volume volumeSlider').append(G.make('nightSprite'))
 			.append(G.make('sliderTrack litBorder'));
 		slider.handle = G.make('sliderHandle').data('controls', type).appendTo(slider);
 		return slider;
 	}
-	
+
 	function setVolume(handle, v) {
 		var totalWidth = handle.parent().find('.sliderTrack').width();
 		handle.css('transform', 'translate3d(' + (v * totalWidth) + 'px, 0px, 0px)');
 		handle.data('volume', v);
 	}
-	
+
 	var dragTarget = null;
 	function handleTouchStart(e) {
 		// Handle wacky touch event objects
-		if(e.originalEvent.changedTouches) {
+		if (e.originalEvent.changedTouches) {
 			e = e.originalEvent.changedTouches[0];
 		}
 		dragTarget = $(e.target);
 	}
-	
+
 	function handleTouchEnd(e) {
 		// Handle wacky touch event objects
-		if(e.originalEvent.changedTouches) {
+		if (e.originalEvent.changedTouches) {
 			e = e.originalEvent.changedTouches[0];
 		}
-		if(dragTarget != null) {
-			if(dragTarget._cachedOffsetX == null) {
+		if (dragTarget != null) {
+			if (dragTarget._cachedOffsetX == null) {
 				dragTarget._cachedOffsetX = dragTarget.parent().find('.sliderTrack').offset().left;
 				dragTarget._cachedOffsetWidth = dragTarget.parent().find('.sliderTrack').width();
 			}
 			var pos = xPos3d.exec(dragTarget.css('transform'));
-			if(pos == null) {
+			if (pos == null) {
 				pos = xPos.exec(dragTarget.css('transform'));
 			}
-			if(pos != null) {
+			if (pos != null) {
 				var volume = parseFloat(pos[1]) / dragTarget._cachedOffsetWidth;
 			}
 
-			switch(dragTarget.data('controls')) {
-			case 'music':
-				E.trigger('setMusicVolume', [volume]);
-				break;
-			case 'effects':
-				E.trigger('setEffectsVolume', [volume]);
+			switch (dragTarget.data('controls')) {
+				case 'music':
+					E.trigger('setMusicVolume', [volume]);
+					break;
+				case 'effects':
+					E.trigger('setEffectsVolume', [volume]);
 			}
 		}
 		dragTarget = null;
 	}
-	
+
 	function handleTouchMove(e) {
 		// Handle wacky touch event objects
-		if(e.originalEvent.changedTouches) {
+		if (e.originalEvent.changedTouches) {
 			e = e.originalEvent.changedTouches[0];
 		}
-		if(dragTarget != null) {
-			if(dragTarget._cachedOffsetX == null) {
+		if (dragTarget != null) {
+			if (dragTarget._cachedOffsetX == null) {
 				dragTarget._cachedOffsetX = dragTarget.parent().find('.sliderTrack').offset().left;
 				dragTarget._cachedOffsetWidth = dragTarget.parent().find('.sliderTrack').width();
 			}
 			var pos = e.pageX - dragTarget._cachedOffsetX;
-			if(pos < 0) {
+			if (pos < 0) {
 				pos = 0;
-			} else if(pos > dragTarget._cachedOffsetWidth) {
+			} else if (pos > dragTarget._cachedOffsetWidth) {
 				pos = dragTarget._cachedOffsetWidth;
 			}
 			dragTarget.css('transform', 'translate3d(' + (pos) + 'px, 0px, 0px)');
@@ -95,11 +95,11 @@ define(['app/eventmanager', 'app/gamestate'], function(E, State) {
 		setVolume(musicHandle, require('app/gameoptions').get('musicVolume'));
 		setVolume(effectsHandle, require('app/gameoptions').get('effectsVolume'));
 	}
-	
+
 	return {
-		init: function() {
+		init: function () {
 			G = require('app/graphics/graphics');
-			if(_el) {
+			if (_el) {
 				_el.remove();
 			}
 			_el = null;
